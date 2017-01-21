024a50841c2e8f610fa999495dd93ad54d6c8b4b
Eduardo Seabra <dududuedu.seabra@gmail.com>
2017-01-21 00:01:39 -0200

    font preference

--- copy-files.sh
+++ copy-files.sh
@@ -6,7 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $BIN_FOLDER
 cp -R $DIR/bin/* "$BIN_FOLDER"
 cp -R "$DIR/fish/config.fish" "$HOME/.config/fish"
-cp -R "$DIR/i3/.i3" "$HOME/.config/i3"
 cp -R "$DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
 cp -R $DIR/vim/.vim/* "$HOME/.vim"
 cp -R "$DIR/vim/.vimrc" "$HOME/.vimrc"
+
+cp -R "$DIR/i3/.i3" "$HOME/.config/i3"
+sudo cp "$DIR/i3/i3status.conf" "/etc/i3status.conf"
