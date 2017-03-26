533c169497594114bc3d4e82e2503e10c8a16626
Eduardo Seabra <dududuedu.seabra@gmail.com>
2017-03-26 14:55:57 -0300

    debian install

--- dl-dependencies.sh
+++ dl-dependencies.sh
@@ -5,26 +5,27 @@
 BIN_FOLDER="$HOME/bin"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
+
 apt update
 apt install gcc\
-                 emacs vim xclip\
-                 python-pip\
-                 sshpass\
-                 fish\
-                 git\
-                 tmux\
-                 dmenu i3 i3status i3lock xbacklight feh\
-                 vlc mpg123 ffmpeg\
-                 gdb wireshark radare2\
-                 pavucontrol\
-                 htop\
-                 redshift\
-                 ranger w3m caca-utils\
-                 evince\
-                 cloc
+            emacs vim xclip\
+            python-pip\
+            sshpass\
+            git\
+            tmux\
+            dmenu i3 i3status i3lock xbacklight feh\
+            vlc mpg123 \
+            gdb wireshark radare2\
+            pavucontrol\
+            htop\
+            redshift\
+            ranger w3m caca-utils\
+            youtube-dl\
+            evince\
+            cloc
 
-apt purge xserver-xorg-video-ati xserver-xorg-video-radeon
-./$DIR/fix-intel.sh
+apt install -t jessie-backports openjdk-8-jdk
 
 if [ ! -d $BIN_FOLDER ]; then
     mkdir -p $BIN_FOLDER
@@ -36,8 +37,6 @@ wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -P 
 chmod +x "$BIN_FOLDER/lein"
 wget https://raw.githubusercontent.com/slimm609/checksec.sh/master/checksec -P "$BIN_FOLDER"
 chmod +x "$BIN_FOLDER/checksec"
-
-sudo pip install --upgrade youtube_dl
 
 git clone https://github.com/longld/peda.git "$HOME/peda"
 echo "source ~/peda/peda.py" >> ~/.gdbinit
