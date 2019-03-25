801a109bdd650c247cd83ccfad01d802e5521e8c
Eduardo Seabra <dududuedu.seabra@gmail.com>
2019-03-25 20:57:31 -0300

    Install root configuration files

--- install-configs.sh
+++ install-configs.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-PACKAGES_DIR="config"
+function install_packages {
+  as_root="$1"
+  source_dir="$2"
+  dest_dir="$3"
+  for package in `ls $source_dir`;
+  do
+      echo "installing package: $package"
+      cmd="stow -d '$source_dir' -t '$dest_dir' --verbose=1 -v '$package'"
+      if $as_root ; then
+         sudo sh -c "$cmd"
+      else
+         eval $cmd
+      fi
+  done
+}
 
-for package in `ls $PACKAGES_DIR`;
-do
-    echo "installing package: $package"
-    stow -d $PACKAGES_DIR -t $HOME --verbose=1 $package
-done
-
+install_packages false "config" "$HOME"
+install_packages true "./root_config" "/"
