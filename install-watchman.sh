f2ea27279c1efb0c6dc9940d876c7296c05f27a3
Eduardo Seabra <dududuedu.seabra@gmail.com>
2019-03-31 15:27:59 -0300

    Remove watchman

--- install-watchman.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt install inotify-tools build-essential libssl-dev libcrypto++-dev pkg-config libtool python-dev automake autoconf
-git clone https://github.com/facebook/watchman.git
-cd watchman
-git checkout v4.9.0  # the latest stable release
-./autogen.sh
-./configure
-make
-sudo make install
